e60cf9dc7de33904814785a4b97cad395f864ecb
Marcos Djivelekian <djmarcos88@gmail.com>
2019-09-17 11:30:21 -0300

    Solved maxchar and fizzbuzz

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,21 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+    for (let i = 1; i <= n; i++) {
+        const mul3 = i % 3 === 0;
+        const mul5 = i % 5 === 0;
+
+        if (mul3 && mul5) {
+            console.log('fizzbuzz');
+        } else if (mul3) {
+            console.log('fizz');
+        } else if (mul5) {
+            console.log('buzz');
+        } else {
+            console.log(i);
+        }
+    }
+}
 
 module.exports = fizzBuzz;
--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,45 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let result = '';
+    let counter = new Map();
 
-module.exports = maxChar;
+    for (let char of str) {
+        const val = counter.get(char) || 0;
+
+        counter.set(char, val + 1);
+    }
+
+    let max = 0;
+
+    counter.forEach((val, key) => {
+        if (val > max) {
+            max = val;
+            result = key;
+        }
+    });
+
+    return result;
+}
+
+function maxChar2(str) {
+    let result = '';
+    let counter = new Map();
+    let max = 0;
+
+    for (let char of str) {
+        const val = counter.get(char) + 1 || 0;
+
+        counter.set(char, val);
+
+        if (val > max) {
+            max = val;
+            result = char;
+        }
+    }
+
+    return result !== '' ? result : str[0];
+}
+
+module.exports = maxChar2;
